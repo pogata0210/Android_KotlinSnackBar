686399fa4235aa5eabb239a1224eb184f6ac9974
Pablo Ogata <pabloogata@gmail.com>
2019-04-01 18:08:25 -0400

    assig val

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main (args: Array<String>){
 
+    val jane = Customer(1,"Jane", 45.25 )
+    val bob = Customer(2, "Bob", 33.14)
+
+    val food = VendingMachine(1, "food")
+    val drink = vendingMachine(2, "drink")
+    val office = vendingMachine(3, "office")
+
+    val chips = Snack(1, "Chips", 36, 1.75, 1)
+    val chocolate = Snack(2, "Chocolate Bar", 36, 1.00, 1)
+    val pretzel = Snack(3, "Pretzel", 30, 2.00, 1)
+
+    val soda = Snack(1, "Soda", 24, 2.50, 2)
+    val water = Snack(2, "Water", 20, 2.75, 2)
+
+
 }
